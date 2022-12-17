552be6f033e64b73b982518c0ed3d9da0f635a55
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-17 16:36:49 +0800

    [Add] ok result

--- DotnetRPG/Controllers/CharacterController.cs
+++ DotnetRPG/Controllers/CharacterController.cs
@@ -9,4 +9,8 @@ public class CharacterController : ControllerBase
 {
     private static Character knight = new Character();
 
+    public IActionResult Get()
+    {
+        return Ok(knight);
+    }
 }

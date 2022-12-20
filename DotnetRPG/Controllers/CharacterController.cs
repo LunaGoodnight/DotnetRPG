f3ad5fb345f24c5113bf65d07481efd196b56996
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-20 16:32:33 +0800

    [Add] HttpGet attribute

--- DotnetRPG/Controllers/CharacterController.cs
+++ DotnetRPG/Controllers/CharacterController.cs
@@ -9,6 +9,7 @@ public class CharacterController : ControllerBase
 {
     private static Character knight = new Character();
 
+    [HttpGet]
     public IActionResult Get()
     {
         return Ok(knight);
